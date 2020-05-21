12de5a10a6437cb42325f8e4517a422aba1a271f
Dan <danedupadilla@protonmail.com>
2020-05-21 15:18:06 -0500

    Updated schema to include password field for users.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,4 +2,11 @@ class Api::UsersController < ApplicationController
     def welcome
         render "users.json.jbuilder"
     end
+
+    def create
+        @user: User.new(
+            
+        )
+
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_154724) do
+ActiveRecord::Schema.define(version: 2020_05_21_201407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_154724) do
     t.integer "doggo_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
